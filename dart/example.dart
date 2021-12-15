929dd6d6eb06bc8ec027cddbbbde72d66fb0bfd9
SouL-H <mhmtaydinw@gmail.com>
2021-12-15 21:22:16 +0300

    class exchange

--- dart/example.dart
+++ dart/example.dart
@@ -3,15 +3,17 @@ class Example {
   //private
   int _age;
   String? school;
+  Example? benchmate;
 
-  //Late ile değer atayacağımıza söz vermiş oluyoruz. 
+  //Late ile değer atayacağımıza söz vermiş oluyoruz.
   //Hata yazmaz fakat değer atamadan çalıştırdığımızda uygulama problem oluşturur.
   late String adress;
 
-  Example(this.name, this._age, this.school,this.adress);
+  Example(this.name, this._age, this.school, this.adress);
 
   void StudentInfo() {
     print("Student name: $name and age: $_age school: $school adress: $adress");
+    if (benchmate != null) print("Sira arkadasi: "+(benchmate!.name));
   }
 
   void ageUp() {
---
+++ dart/example2.dart
@@ -0,0 +1,14 @@
+/*void main() {
+
+
+
+}
+
+class Student {
+  final String name;
+  //Sadece bir kez değer atayacağız boş değer verdiğimizde problem yaratır.
+  //late final int age;
+  final int age;
+
+  Student(this.name, this.age);
+}*/
