22dff85e7917e95fc58efcce7583170d3e9f08ff
SouL-H <mhmtaydinw@gmail.com>
2021-12-15 21:21:37 +0300

    Capsulation V2

--- dart/enCapsulation.dart
+++ dart/enCapsulation.dart
@@ -1,11 +1,9 @@
-
-
-
 import 'example.dart';
 
 void main() {
-  Example exp1 = Example("Ahmet", 18,"test1","a");
-  Example exp2 = Example("Murat", 27,"test2","n");
+  Example exp1 = Example("Ahmet", 18, "test1", "a");
+  Example exp2 = Example("Murat", 27, "test2", "n");
+  Example exp3 = Example("Ömer", 28, "test3", "n");
 
   exp1.StudentInfo();
   exp2.StudentInfo();
@@ -13,4 +11,10 @@ void main() {
   exp1.ageUp();
   exp1.ageUp();
   exp1.StudentInfo();
+
+  exp1.benchmate = exp2;
+  exp2.benchmate = exp1;
+  exp1.StudentInfo();
+  exp2.StudentInfo();
+  exp3.StudentInfo();
 }
