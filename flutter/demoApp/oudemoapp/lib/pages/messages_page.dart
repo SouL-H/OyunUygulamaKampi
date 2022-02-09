83ce307c5afb829bac0461bd3125e8c2108d1d19
SouL-H <mhmtaydinw@gmail.com>
2022-02-10 00:34:45 +0300

    messages pages design

--- flutter/demoApp/oudemoapp/lib/pages/messages_page.dart
+++ flutter/demoApp/oudemoapp/lib/pages/messages_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class messagesPage extends StatefulWidget {
@@ -12,16 +14,69 @@ class _messagesPageState extends State<messagesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Mesajlar")),
-        body: ListView.builder(itemBuilder: ((context, index) {
-          bool benMiyim = true;
-          return Align(alignment: Alignment.centerRight,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              border: Border.all(color:Colors.black, width: 2), 
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  reverse: true, //son kısmından başlıyor.
+                  itemBuilder: ((context, index) {
+                    bool benMiyim = Random().nextBool();
+                    return Align(
+                        alignment: benMiyim
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 16.0),
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Colors.orange.shade200,
+                              border: Border.all(color: Colors.grey, width: 2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.all(24.0),
+                              child: Text("Mesaj"),
+                            ),
+                          ),
+                        ));
+                  })),
             ),
-            child: Text("Mesaj"),
-          )
-          );
-        })));
+            DecoratedBox(
+              decoration: BoxDecoration(border: Border.all()),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(15.0))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder
+                                  .none), //Giriş alt çizgi kaldırdık.
+                        ),
+                      ),
+                    ),
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          print("Helö");
+                        },
+                        child: const Text("Gönder")),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
